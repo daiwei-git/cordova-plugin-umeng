836287ea49b83585054ad351e103e3ec924ade16
daiwei <1250194587@qq.com>
2022-08-11 10:13:20 +0800

    调整名称

--- www/UMSDK.js
+++ www/UMSDK.js
@@ -2,62 +2,62 @@ var exec = require('cordova/exec');
 
 module.exports = {
     init: function(appKey, channelId, deviceType, pushSecret) {
-        exec(null, null, "Umeng", "init", [ 
+        exec(null, null, "UMSDK", "init", [ 
             appKey, channelId, deviceType, pushSecret
         ]);
     },
     onEvent: function(eventId) {
-        exec(null, null, "Umeng", "onEvent", [ 
+        exec(null, null, "UMSDK", "onEvent", [ 
             eventId 
         ]);
     },
     onEventWithLabel: function(eventId, eventLabel) {
-        exec(null, null, "Umeng", "onEventWithLabel", [ 
+        exec(null, null, "UMSDK", "onEventWithLabel", [ 
             eventId, eventLabel 
         ]);
     },
     onEventWithParameters: function(eventId, eventData) {
-        exec(null, null, "Umeng", "onEventWithParameters", [ 
+        exec(null, null, "UMSDK", "onEventWithParameters", [ 
             eventId, eventData 
         ]);
     },
     onEventWithCounter: function(eventId, eventData, eventNum) {
-        exec(null, null, "Umeng", "onEventWithCounter", [ 
+        exec(null, null, "UMSDK", "onEventWithCounter", [ 
             eventId, eventData, eventNum 
         ]);
     },
     onPageBegin: function(pageName) {
-        exec(null, null, "Umeng", "onPageBegin", [ 
+        exec(null, null, "UMSDK", "onPageBegin", [ 
             pageName 
         ]);
     },
     onPageEnd: function(pageName) {
-        exec(null, null, "Umeng", "onPageEnd", [ 
+        exec(null, null, "UMSDK", "onPageEnd", [ 
             pageName 
         ]);
     },
     getDeviceId: function(callBack) {
-        exec(callBack, null, "Umeng", "getDeviceId", []);
+        exec(callBack, null, "UMSDK", "getDeviceId", []);
     },
     getDeviceInfo: function(callBack) {
-        exec(callBack, null, "Umeng", "getDeviceInfo", []);
+        exec(callBack, null, "UMSDK", "getDeviceInfo", []);
     },
     setLogEnabled: function(enabled) {
-        exec(null, null, "Umeng", "setLogEnabled", [ 
+        exec(null, null, "UMSDK", "setLogEnabled", [ 
             enabled 
         ]);
     },
     profileSignInWithPUID: function(UID) {
-        exec(null, null, "Umeng", "profileSignInWithPUID", [ 
+        exec(null, null, "UMSDK", "profileSignInWithPUID", [ 
             UID
         ]);
     },
     profileSignInWithPUIDWithProvider: function(UID, provider) {
-        exec(null, null, "Umeng", "profileSignInWithPUIDWithProvider", [ 
+        exec(null, null, "UMSDK", "profileSignInWithPUIDWithProvider", [ 
             UID, provider 
         ]);
     },
     profileSignOff: function() {
-        exec(null, null, "Umeng", "profileSignOff", []);
+        exec(null, null, "UMSDK", "profileSignOff", []);
     }
 };
